cf2d5660213d4c8ea567939dc5458d3463d02e37
Tasztalos69 <tasztalos69@gmail.com>
2019-07-26 17:50:07 +0200

    Landing-ART

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 //import PropTypes from "prop-types";
 
+import { ReactComponent as ARTleft } from "../assets/Landing-left.svg";
+import { ReactComponent as ARTright } from "../assets/Landing-right.svg";
+import { ReactComponent as ARTbtn } from "../assets/Landing-btn-border.svg";
+
 import "../css/landing.css";
 import "../css/fonts.css";
 
@@ -15,6 +19,8 @@ export default class Landing extends Component {
 					<h3>Legutóbb hozzáadott:</h3>
 					<h1>placeholder - original mix</h1>
 				</div>
+				<ARTleft className="art-left" />
+				<ARTright className="art-right" />
 			</div>
 		);
 	}
