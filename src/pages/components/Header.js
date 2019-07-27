31cff500243b2afbefb9359289ecd16b5190cb8d
Tasztalos69 <tasztalos69@gmail.com>
2019-07-27 14:21:52 +0200

    Add favicon + Header state management

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -6,12 +6,25 @@ import "../../css/header.css";
 import { ReactComponent as MusicPlayer } from "../../assets/music-player.svg";
 
 export default class Header extends Component {
+	state = {
+		showUpright: false,
+		showUpleft: false
+	};
+
+	toggleUpright = e => {
+		this.setState({ showUpright: !this.state.showUpright });
+	};
+
+	toggleUpleft = e => {
+		this.setState({ showUpleft: !this.state.showUpleft });
+	};
+
 	render() {
 		const kolcsey = this.props.kolcsey;
 		return (
 			<div>
-				<MusicPlayer className="menu-upleft" />
-				<div className="menu-upright" onClick="">
+				<MusicPlayer className="menu-upleft" onClick={this.toggleUpleft} />
+				<div className="menu-upright" onClick={this.toggleUpright}>
 					<span className="mspan" />
 					<span className="mspan" />
 					<span className="mspan" />
