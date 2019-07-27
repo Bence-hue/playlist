653b8de4d96641d5869d7b0ba48901f8bc5b1169
btoth04 <20687234+btoth04@users.noreply.github.com>
2019-07-27 22:15:29 +0200

    played api

--- playlist/frontend/views.py
+++ playlist/frontend/views.py
@@ -8,6 +8,6 @@ def index_view(request, *args,**kwargs):
     response=HttpResponse('valami')
     if 'userid' not in request.COOKIES:
         max_age = 4 * 365 * 24 * 60 * 60
-        expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+        expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
         response.set_cookie('userid', str(uuid.uuid4()), max_age=max_age, expires=expires)
     return response
--- playlist/list/urls.py
+++ playlist/list/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns=[
-    path('new/',views.new_view,name='new')
+    path('new/',views.new_view,name='new'),
+    path('played/',views.played_view,name="played")
 ]
