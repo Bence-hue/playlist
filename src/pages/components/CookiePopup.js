84ed9028d688ba44f0fbc673f5fcb1cd4a7e873c
Tasztalos69 <tasztalos69@gmail.com>
2019-07-28 21:51:25 +0200

    Add cookie animation

--- src/pages/components/CookiePopup.js
+++ src/pages/components/CookiePopup.js
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
+import { Spring } from "react-spring/renderprops";
 
 import "../../css/cookie.css";
+import "../../css/hover.css";
 
 export default class CookiePopup extends Component {
 	render() {
 		const hideCookie = this.props.hideCookie;
 		return (
-			<div className="cookie-popup-back">
-				<div className="cookie-popup">
-					<div className="cookie-wrapper">
-						<h1>
-							<span>Süti beállítások. - </span>Unalmas. Fárasztó. De kötelező.
-						</h1>
-						<p>
-							Az oldal használatával elfogadod azt, hogy mi az oldal
-							üzemeltetéséhez Sütiket (cookie-kat) használunk.
-						</p>
-						<button onClick={hideCookie}>Értem!</button>
+			<Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+				{props => (
+					<div style={props}>
+						<div className="cookie-popup-back">
+							<div className="cookie-popup">
+								<div className="cookie-wrapper">
+									<h1>
+										<span>Süti beállítások. - </span>Unalmas. Fárasztó. De
+										kötelező.
+									</h1>
+									<p>
+										Az oldal használatával elfogadod azt, hogy mi az oldal
+										üzemeltetéséhez Sütiket (cookie-kat) használunk.
+									</p>
+									<button onClick={hideCookie} className="hvr-grow">
+										Értem!
+									</button>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				)}
+			</Spring>
 		);
 	}
 }
