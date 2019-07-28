e921b3531f23c7d7e9a7211e9a785c4119ffb027
btoth04 <20687234+btoth04@users.noreply.github.com>
2019-07-28 21:29:42 +0200

    new, list (mode 0), delete, played works

--- playlist/playlist/settings.py
+++ playlist/playlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's@-enklwz=2of06e1$!jbc7*p&wt=qha@jwo421!xnp-7z)&w0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["46.107.123.236","127.0.0.1"]
 
 
 # Application definition
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'list'
+    'list',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL=True
