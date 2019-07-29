dce1ac715b6295b2de18df9698dca9f4a38a9bcb
btoth04 <20687234+btoth04@users.noreply.github.com>
2019-07-29 12:23:37 +0200

    react placed in django server

--- playlist/frontend/views.py
+++ playlist/frontend/views.py
@@ -5,9 +5,10 @@ import datetime
 
 # Create your views here.
 def index_view(request, *args,**kwargs):
-    response=HttpResponse('valami')
-    if 'userid' not in request.COOKIES:
-        max_age = 4 * 365 * 24 * 60 * 60
-        expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-        response.set_cookie('userid', str(uuid.uuid4()), max_age=max_age, expires=expires)
-    return response
+    # response=HttpResponse('valami')
+    # if 'userid' not in request.COOKIES:
+    #     max_age = 4 * 365 * 24 * 60 * 60
+    #     expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    #     response.set_cookie('userid', str(uuid.uuid4()), max_age=max_age, expires=expires)
+    # return response
+    return render(request,"index.html")
