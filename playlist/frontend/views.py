48b0eaedc0b44d2073559d3012da1d8bdbb15d3d
btoth04 <20687234+btoth04@users.noreply.github.com>
2019-07-29 13:14:40 +0200

    userid cookie  added, runserver.bat created

--- playlist/frontend/views.py
+++ playlist/frontend/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import uuid
 import datetime
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
-def index_view(request, *args,**kwargs):
-    # response=HttpResponse('valami')
-    # if 'userid' not in request.COOKIES:
-    #     max_age = 4 * 365 * 24 * 60 * 60
-    #     expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-    #     response.set_cookie('userid', str(uuid.uuid4()), max_age=max_age, expires=expires)
-    # return response
-    return render(request,"index.html")
+@ensure_csrrun  f_cookie
+def frontend_view(request, *args,**kwargs):
+    response = render(request, "index.html")
+    if 'userid' not in request.COOKIES:
+        max_age = 4 * 365 * 24 * 60 * 60
+        expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+        response.set_cookie('userid', str(uuid.uuid4()), max_age=max_age, expires=expires)
+    return response
---
+++ runserver.bat
@@ -0,0 +1,2 @@
+copy build playlist\frontend\build
+pipenv run python playlist\manage.py runserver 0.0.0.0:8000
