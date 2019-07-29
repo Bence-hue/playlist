f672ea52c643c652d00607f7b1d84e3c9e77d66a
Tasztalos69 <tasztalos69@gmail.com>
2019-07-30 00:44:54 +0200

    Manual page switching on /songs

--- src/pages/ListSongs.js
+++ src/pages/ListSongs.js
@@ -4,32 +4,47 @@ import axios from "axios";
 import Header from "./components/Header";
 import "../css/songlist.css";
 
-import SongCard from "./components/SongCard";
+import SongListPage from "./components/SongListPage";
 import UpcomingCard from "./components/UpcomingCard";
 
 export default class ListSongs extends Component {
 	state = {
-		songs: []
+		songs: [],
+		currentPage: 0
 	};
 
 	componentDidMount() {
-		let url = "http://46.107.123.236:8000/api/list/?mode=unplayed";
+		let url = "http://bnctth.ml:8000/api/list/?mode=unplayed";
 		axios.get(url).then(res => this.setState({ songs: res.data }));
 	}
 
 	render() {
-		const { songs } = this.state;
+		const { songs, currentPage } = this.state;
 		const getIds = () => {
 			return songs.map(d => d.id);
 		};
 		const smallestId = Math.min(...getIds());
 
-		const otherSongs = songs.filter(sg => {
-			return sg.id !== smallestId;
-		});
 		const upcomingSong = songs.filter(sg => {
 			return sg.id === smallestId;
 		});
+		const sl_Round = Math.ceil(songs.length / 5);
+
+		let songPages = [];
+		for (let i = 0; i < sl_Round; i++) {
+			if (i === 0) {
+				songPages.push(<SongListPage key={i} id={i} isFirstPage={true} />);
+			}
+			if (i !== 0) {
+				songPages.push(<SongListPage key={i + 1} id={i} isFirstPage={false} />);
+			}
+		}
+		let songCardWrapperStyle;
+		if (currentPage === 0) {
+			songCardWrapperStyle = { marginTop: "120px" };
+		} else {
+			songCardWrapperStyle = { marginTop: "0" };
+		}
 
 		return (
 			<React.Fragment>
@@ -40,12 +55,13 @@ export default class ListSongs extends Component {
 					</h1>
 					<div className="list-wrapper">
 						{upcomingSong.map(sg => {
-							return <UpcomingCard key={sg.id} song={sg} />;
+							if (currentPage === 0) {
+								return <UpcomingCard key={sg.id} song={sg} />;
+							}
+							return null;
 						})}
-						<div className="song-card-wrapper">
-							{otherSongs.map(sg => {
-								return <SongCard key={sg.id} song={sg} />;
-							})}
+						<div className="song-card-wrapper" style={songCardWrapperStyle}>
+							{songPages[currentPage]}
 						</div>
 					</div>
 				</div>
---
+++ src/pages/components/SongListPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import SongCard from "./SongCard";
+
+export default class SongListPage extends Component {
+	state = {
+		songs: []
+	};
+
+	componentDidMount() {
+		let url = "http://bnctth.ml:8000/api/list/?mode=unplayed";
+		axios.get(url).then(res => this.setState({ songs: res.data }));
+	}
+	render() {
+		const { songs } = this.state;
+		const getIds = () => {
+			return songs.map(d => d.id);
+		};
+		const smallestId = Math.min(...getIds());
+
+		const otherSongs = songs.filter(sg => {
+			return sg.id !== smallestId;
+		});
+
+		let sliceStart, sliceStop;
+		if (this.props.isFirstPage) {
+			sliceStart = this.props.id * 5;
+			sliceStop = this.props.id * 5 + 4;
+		} else {
+			sliceStart = this.props.id * 5 - 1;
+			sliceStop = this.props.id * 5 + 4;
+		}
+
+		return (
+			<div>
+				{/* <h1>{`${sliceStart} ${sliceStop}`}</h1> */}
+				{otherSongs.slice(sliceStart, sliceStop).map(sg => {
+					return <SongCard key={sg.id} song={sg} />;
+				})}
+			</div>
+		);
+	}
+}
