f549390952fb187d7f1908399ae871e193ef5dc0
Tasztalos69 <tasztalos69@gmail.com>
2019-07-30 13:17:53 +0200

    render preloader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Preloader, Placeholder } from "react-preloading-screen";
 
 import Landing from "./pages/Landing";
 import ListSongs from "./pages/ListSongs";
@@ -8,14 +9,20 @@ import NewSong from "./pages/NewSong";
 function App() {
 	return (
 		<Router>
-			<div className="App">
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/songs" component={ListSongs} />
-					<Route exact path="/new" component={NewSong} />
-					<Route component={""} />
-				</Switch>
-			</div>
+			<Preloader>
+				<Placeholder>
+					<h1>Loading</h1>
+				</Placeholder>
+
+				<div className="App">
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/songs" component={ListSongs} />
+						<Route exact path="/new" component={NewSong} />
+						<Route component={""} />
+					</Switch>
+				</div>
+			</Preloader>
 		</Router>
 	);
 }
