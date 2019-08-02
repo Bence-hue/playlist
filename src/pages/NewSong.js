cf41e79fab4dcced467f196dcbe17cab1e745beb
Tasztalos69 <tasztalos69@gmail.com>
2019-08-02 18:07:06 +0200

    Add dash / git branch test

--- src/pages/NewSong.js
+++ src/pages/NewSong.js
@@ -4,13 +4,13 @@ import Header from "../pages/components/Header";
 
 import "../css/newsong.css";
 
+import dash1 from "../assets/Dash-trans-1.png";
+import dash2 from "../assets/Dash-trans-2.png";
+import dash3 from "../assets/Dash-trans-3.png";
+import dash4 from "../assets/Dash-trans-4.png";
+import dash5 from "../assets/Dash-trans-5.png";
+
 import { ReactComponent as NEWb } from "../assets/add-button.svg";
-import { ReactComponent as NEW0 } from "../assets/NEW-0-outer.svg";
-import { ReactComponent as NEW1 } from "../assets/NEW-1.svg";
-import { ReactComponent as NEW2 } from "../assets/NEW-2.svg";
-import { ReactComponent as NEW3 } from "../assets/NEW-3.svg";
-import { ReactComponent as NEW4 } from "../assets/NEW-4.svg";
-import { ReactComponent as NEW5 } from "../assets/NEW-5.svg";
 import { ReactComponent as NartLeft } from "../assets/new-song-left.svg";
 import { ReactComponent as NartRight } from "../assets/new-song-right.svg";
 
@@ -21,13 +21,11 @@ export default class NewSong extends Component {
 				<Header kolcsey={false} />
 				<NEWb className="new-song-button" />
 				<h2 className="new-song-h2">Új zenét akarok!</h2>
-				<NEW0 className="ARTnew new0" />
-				{/* <NEW1 className="ARTnew new1" /> */}
-				<NEW1 className="ARTnew new1--green" />
-				<NEW2 className="ARTnew new2" />
-				<NEW3 className="ARTnew new3" />
-				<NEW4 className="ARTnew new4" />
-				<NEW5 className="ARTnew new5" />
+				<img src={dash1} alt="dash1" className="dash1" />
+				<img src={dash2} alt="dash2" className="dash2" />
+				<img src={dash3} alt="dash3" className="dash3" />
+				<img src={dash4} alt="dash4" className="dash4" />
+				<img src={dash5} alt="dash5" className="dash5" />
 				<NartLeft className="new-art-left" />
 				<NartRight className="new-art-right" />
 			</div>
