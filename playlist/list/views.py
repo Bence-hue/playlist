06048e8177035c89260bccd14aafa14321c13d69
btoth04 <20687234+btoth04@users.noreply.github.com>
2019-08-05 12:00:38 +0200

    token enabled

--- playlist/list/views.py
+++ playlist/list/views.py
@@ -19,45 +19,45 @@ with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)
 @csrf_exempt
 def new_view(request, *args, **kwargs):
     if request.method == 'POST':
-        # if request.POST.get("token", "") == config["token"]:
-        data=request.POST
-        print(data)
-        URL="https://www.googleapis.com/youtube/v3/search"
-        song=data.get("artist","")+" - "+data.get("title","")
-        PARAMS={
-            "part":"snippet",
-            "key": config.get("YTAPIKEY",""),
-            "type":"video",
-            "maxResults":1,
-            "q":song
-        }
-        r=requests.get(url = URL, params = PARAMS)
-        respons=r.json()
-        print(respons)
-        try:
-            link='https://youtube.com/watch?v='+respons["items"][0]["id"]["videoId"]
-            yttitle=respons["items"][0]["snippet"]["title"]
-        except:
-            link=""
-            yttitle=""
-        print(link)
-        user=request.COOKIES.get("userid","XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
-        lastrecord=Song.objects.filter(createdAt__gte=timezone.now()-datetime.timedelta(seconds=15),user=user)
-        if not lastrecord.exists():
-            if not Song.objects.filter(link=link,played=False).exists():
-                if not Song.objects.filter(link=link,played=True, playedAt__gte=timezone.now()-datetime.timedelta(minutes=1)).exists():
-                    Song.objects.create(title=data.get("title"),artist=data.get("artist"),link=link,user=user,yttitle=yttitle)
-                    return HttpResponse(status=201)
-                else: #ha az utobbi egy hetben lett lejatszva
-                    return HttpResponse("{\"played\": True}", status=422)
-            else: #ha van meg le nem jatszott ilyen
-                return HttpResponse("{\"played\":False}",status=422)
-        else: #ha az utobbi 15 percben kuldott
-            remaining = int((datetime.timedelta(minutes=15)-(timezone.now()-lastrecord.get().createdAt)).total_seconds())
-            print(remaining)
-            return HttpResponse(str(int(remaining/60))+":"+"{:02d}".format(remaining % 60), status=429)
-        # else:
-        #     return HttpResponse("INVALID TOKEN",status=403)
+        if request.POST.get("token", "") == config["token"]:
+            data=request.POST
+            print(data)
+            URL="https://www.googleapis.com/youtube/v3/search"
+            song=data.get("artist","")+" - "+data.get("title","")
+            PARAMS={
+                "part":"snippet",
+                "key": config.get("YTAPIKEY",""),
+                "type":"video",
+                "maxResults":1,
+                "q":song
+            }
+            r=requests.get(url = URL, params = PARAMS)
+            respons=r.json()
+            print(respons)
+            try:
+                link='https://youtube.com/watch?v='+respons["items"][0]["id"]["videoId"]
+                yttitle=respons["items"][0]["snippet"]["title"]
+            except:
+                link=""
+                yttitle=""
+            print(link)
+            user=request.COOKIES.get("userid","XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
+            lastrecord=Song.objects.filter(createdAt__gte=timezone.now()-datetime.timedelta(seconds=15),user=user)
+            if not lastrecord.exists():
+                if not Song.objects.filter(link=link,played=False).exists():
+                    if not Song.objects.filter(link=link,played=True, playedAt__gte=timezone.now()-datetime.timedelta(minutes=1)).exists():
+                        Song.objects.create(title=data.get("title"),artist=data.get("artist"),link=link,user=user,yttitle=yttitle)
+                        return HttpResponse(status=201)
+                    else: #ha az utobbi egy hetben lett lejatszva
+                        return HttpResponse("{\"played\": True}", status=422)
+                else: #ha van meg le nem jatszott ilyen
+                    return HttpResponse("{\"played\":False}",status=422)
+            else: #ha az utobbi 15 percben kuldott
+                remaining = int((datetime.timedelta(minutes=15)-(timezone.now()-lastrecord.get().createdAt)).total_seconds())
+                print(remaining)
+                return HttpResponse(str(int(remaining/60))+":"+"{:02d}".format(remaining % 60), status=429)
+        else:
+            return HttpResponse("INVALID TOKEN",status=403)
     else: # ha nem poston kuldott
         return HttpResponse(status=405)
 
