35effa930dcd09e23d60e2e49cfa0ad4520a646c
Tasztalos69 <tasztalos69@gmail.com>
2019-08-05 22:35:03 +0200

    edit s-w .2

--- build/service-worker.js
+++ build/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
 	"https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-importScripts("/static/precache-manifest.f1ff4d740c11c67f6a95fd1bfcbc97e1.js");
+importScripts("/precache-manifest.f1ff4d740c11c67f6a95fd1bfcbc97e1.js");
 
 self.addEventListener("message", event => {
 	if (event.data && event.data.type === "SKIP_WAITING") {
@@ -34,7 +34,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(
-	workbox.precaching.getCacheKeyForURL("/index.html"),
+	workbox.precaching.getCacheKeyForURL("/static/index.html"),
 	{
 		blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
 	}
