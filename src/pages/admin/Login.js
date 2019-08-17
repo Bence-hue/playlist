af9bbfb51030067ed4e2a4d74b1ab03dc0800988
Tasztalos69 <tasztalos69@gmail.com>
2019-08-17 18:38:55 +0200

    admin markup + init sass develpoment

--- src/pages/admin/Login.js
+++ src/pages/admin/Login.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+
+import "../../css/admin-css/login.scss";
 
 export default class AdminLogin extends Component {
+	state = {
+		name: null,
+		pass: null
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+	};
 	render() {
 		return (
 			<div>
-				<h1>Hello World!</h1>
+				<Header kolcsey={true} />
+				<div id="login-wrapper">
+					<h1>
+						<span>Admin</span>
+						<br />
+						Bejelentkezés
+					</h1>
+					<form onSubmit={this.onSubmit}>
+						<input type="text" name="name" placeholder="Felhasználónév" />
+						<input type="password" name="pass" placeholder="Jelszó" />
+						<div className="submit">
+							<i class="fas fa-arrow-right" />
+							<input type="submit" value="" />
+						</div>
+					</form>
+				</div>
 			</div>
 		);
 	}
