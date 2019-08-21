9ab41c5b4a8a06ccf30dd889f26083c6cacd24b2
Tasztalos69 <tasztalos69@gmail.com>
2019-08-21 21:30:10 +0200

    Add sentry.io integration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+	dsn: "https://58c53292f81b413aa0e2d7903c465f82@sentry.io/1537796"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
