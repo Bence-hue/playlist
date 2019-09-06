a1ee82c83689cea1267363b130a06d883cc5dc80
btoth04 <btoth0211@gmail.com>
2019-09-06 19:56:20 +0200

    setting table

--- playlist/list/admin.py
+++ playlist/list/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Song,Question,BlockedUser,BlockedSong
+from .models import Song,Question,BlockedUser,BlockedSong,Setting
 
 # Register your models here.
 admin.site.register(Song)
 admin.site.register(Question)
 admin.site.register(BlockedUser)
 admin.site.register(BlockedSong)
+admin.site.register(Setting)
--- playlist/list/models.py
+++ playlist/list/models.py
@@ -25,3 +25,7 @@ class BlockedSong(models.Model):
     title=models.CharField(max_length=100)
     artist=models.CharField(max_length=100)
     link=models.URLField()
+
+class Setting(models.Model):
+    name=models.CharField(max_length=100)
+    value=models.IntegerField()
