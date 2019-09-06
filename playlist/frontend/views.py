555376d26aceadd8ca7eb4eb33e81c3cb60b0740
btoth04 <btoth0211@gmail.com>
2019-09-06 20:08:48 +0200

    maintenance redirect

--- playlist/frontend/views.py
+++ playlist/frontend/views.py
@@ -1,14 +1,16 @@
 import base64
 import datetime
-import uuid
-import os
 import json
+import os
+import uuid
 
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import ensure_csrf_cookie
 from simplecrypt import decrypt
-from django.contrib.auth import authenticate, login
+
+from list.models import Setting
 
 with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "datas.json"), "r") as cffile:
     config = json.loads(cffile.readline())
@@ -16,15 +18,24 @@ with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)
 # Create your views here.
 @ensure_csrf_cookie
 def frontend_view(request, *args,**kwargs):
-    response = render(request, "index.html")
-    if 'userid' not in request.COOKIES:
-        userid = str(uuid.uuid4())
+    if Setting.objects.get(name="maintenance")==1:
+        redirect("/maintenance")
     else:
-        userid=request.COOKIES["userid"]
-    max_age = 52 * 7 * 24 * 60 * 60
-    expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-    response.set_cookie('userid',userid , max_age=max_age, expires=expires)
-    return response
+        response = render(request, "index.html")
+        if 'userid' not in request.COOKIES:
+            userid = str(uuid.uuid4())
+        else:
+            userid=request.COOKIES["userid"]
+        max_age = 52 * 7 * 24 * 60 * 60
+        expires = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+        response.set_cookie('userid',userid , max_age=max_age, expires=expires)
+        return response
+
+def maintenance_view(request, *args,**kwargs):
+    if Setting.objects.get(name="maintenance")==1:
+        return render(request, "index.html")
+    else:
+        redirect("/")
 
 def admin_view(request,*args,**kwargs):
     if request.user.is_authenticated:
