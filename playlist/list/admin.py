9c836ca77a8e59ebeb6a87afe053e921d2ad10a4
btoth04 <btoth0211@gmail.com>
2019-09-09 17:39:25 +0200

    remove questions import

--- playlist/list/admin.py
+++ playlist/list/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Song,Question,BlockedUser,Setting,Log
+from .models import Song,BlockedUser,Setting,Log
 
 # Register your models here.
 admin.site.register(Song)
