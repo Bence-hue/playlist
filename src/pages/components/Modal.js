3d516edde2813b7af86a979d87473bce462f7eb3
Tasztalos69 <tasztalos69@gmail.com>
2019-09-10 15:25:54 +0200

    Add modal display:none fix

--- src/pages/components/Modal.js
+++ src/pages/components/Modal.js
@@ -5,17 +5,21 @@ import "../../css/modal.scss";
 
 export default class Modal extends Component {
 	state = {
-		visible: 1
+		opacity: 1,
+		display: "block"
 	};
 	hideModal = () => {
-		this.setState({ visible: 0 });
+		this.setState({ opacity: 0 });
+		setTimeout(() => {
+			this.setState({ display: "none" });
+		}, 230);
 	};
 	render() {
 		const { toggler, title, content } = this.props;
-		const { visible } = this.state;
+		const { opacity, display } = this.state;
 		if (toggler) {
 			return (
-				<div id="noauth" style={{ opacity: visible }}>
+				<div id="noauth" style={{ opacity: opacity, display: display }}>
 					<div className="noauth-modal">
 						<div className="noauth-modal__button-wrapper" onClick={this.hideModal}>
 							<Circle className="noauth-modal-button-icon noauth-circle" />
