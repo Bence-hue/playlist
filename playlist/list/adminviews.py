aa870345bb7c25cab36037c9fccfa780f16ddf1c
btoth04 <btoth0211@gmail.com>
2019-09-13 11:15:10 +0200

    Permission misstype

--- playlist/list/adminviews.py
+++ playlist/list/adminviews.py
@@ -53,7 +53,7 @@ def adminlogout_view(request, *args, **kwargs):
 # @csrf_exempt
 def blockuser_view(request, *args, **kwargs):
     if request.method == 'POST':
-        if request.user.has_perm('can_ban'):
+        if request.user.has_perm('list.can_ban'):
             data = request.POST
             if data.get("permanent", "true") == "true":
                 BlockedUser.objects.create(userid=data.get("userid"), permanent=True)
@@ -77,7 +77,7 @@ def blockuser_view(request, *args, **kwargs):
 
 def unblockuser_view(request, *args, **kwargs):
     if request.method == 'POST':
-        if request.user.has_perm('can_ban'):
+        if request.user.has_perm('list.can_ban'):
             print(request.POST.get("userid"))
             user = BlockedUser.objects.filter(userid=request.POST.get("userid", uuid.uuid4))
             p = user.filter(permanent=True)
@@ -186,7 +186,7 @@ def user_blockedFor(l):
 def settings_view(request, *args, **kwargs):
     if request.user.is_authenticated:
         if request.method=='POST':
-            if request.user.has_perm("can_modify_settings"):
+            if request.user.has_perm("list.can_modify_settings"):
                 s=kwargs["s"]
                 if s=="maintenance":
                     setting=Setting.objects.get(name="maintenance")
