97dea7bd80bdee6ea5a3fd6b32853307d242b115
btoth04 <btoth0211@gmail.com>
2019-09-17 15:51:40 +0200

    hide repair

--- playlist/list/songsviews.py
+++ playlist/list/songsviews.py
@@ -135,8 +135,7 @@ def delete_view(request, *args, **kwargs):
             if object.exists():
                 delete(object[0].spotiuri)
                 Log.objects.create(user=request.user,title="deleted",content=object[0].artist+" - "+object[0].title+" (id: "+str(object[0].id)+")")
-                object[0].hide=True
-                object[0].save()
+                object.update(hide=True)
                 return HttpResponse(status=200)
             else:
                 return HttpResponse(status=422)
