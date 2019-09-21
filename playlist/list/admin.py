138ffa9577d796c68e39769a021cf51c28bf5914
Tóth Bence <btoth0211@gmail.com>
2019-09-21 14:08:37 +0200

    gpg test4

--- playlist/list/admin.py
+++ playlist/list/admin.py
@@ -6,5 +6,5 @@ from .models import Song,BlockedUser,Setting,Log,Spotiuser
 admin.site.register(Song)
 admin.site.register(BlockedUser)
 admin.site.register(Setting)
-admin.site.register(Log) 
+admin.site.register(Log)
 admin.site.register(Spotiuser)
