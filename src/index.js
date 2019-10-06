891d882bcd60ed6092f490caf16d8b63e9213431
Tasztalos Pista <tasztalos69@gmail.com>
2019-10-06 23:11:28 +0200

    Add modal ok handler

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ Sentry.init({
 	dsn: "https://97dc1883092644269366ced63680323b@sentry.io/1537907"
 });
 
-// messaging.onMessage((payload) => {
-// 	console.log("onMessage: ", payload);
-// });
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/pages/components/NewSongQuery.js
+++ src/pages/components/NewSongQuery.js
@@ -5,7 +5,7 @@ import cookie from "react-cookies";
 import { Link } from "react-router-dom";
 import * as firebase from "firebase/app";
 import "firebase/messaging";
-import Modal from "./Modal"
+import Modal from "./Modal";
 
 import { ReactComponent as ExCircle } from "../../assets/circle-notch-solid.svg";
 import { ReactComponent as ExMark } from "../../assets/exclamation-solid.svg";
@@ -19,8 +19,8 @@ export default class NewSongQuery extends Component {
 		flavorspan: "előadóját",
 		artist: "",
 		title: "",
-		toggleAnim: false,
-		toggleFinal: true,
+		toggleAnim: true,
+		toggleFinal: false,
 		toggleErr: false,
 		err: {
 			title: "",
@@ -78,45 +78,6 @@ export default class NewSongQuery extends Component {
 								toggleErr: false
 							});
 						}, 1000);
-						const firebaseConfig = {
-							apiKey: "AIzaSyCSnUYWR95Go_re_d6ClkK0AXgCXJJ5gNI",
-							authDomain: "playlist-248218.firebaseapp.com",
-							databaseURL: "https://playlist-248218.firebaseio.com",
-							projectId: "playlist-248218",
-							storageBucket: "",
-							messagingSenderId: "687275559983",
-							appId: "1:687275559983:web:12a27164caeadf2c3f8ada",
-							measurementId: "G-HZRTRM0P1J"
-						};
-						firebase.initializeApp(firebaseConfig);
-						const messaging = firebase.messaging();
-						messaging.usePublicVapidKey(
-							"BEumBgeaNS-cDLgwwjnUqKfKJs9l10mDn1-99N9RXY-0LaWGUA3I5vB_80k6jKWp2A61NzeM4siW6e1kF3uyzjc"
-						);
-						messaging
-							.requestPermission()
-							.then(() => {
-								console.log("Perm granted");
-								return messaging.getToken();
-							})
-							.then((token) => {
-								let url = "/api/setFcmToken/",
-									params = new URLSearchParams();
-								params.append("token", token);
-								axios.defaults.xsrfCookieName = "csrftoken";
-								axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-								axios
-									.post(url, params)
-									.then((res) => {
-										console.log("Token set");
-									})
-									.catch((err) => {
-										console.log(err);
-									});
-							})
-							.catch((err) => {
-								console.log(err);
-							});
 					})
 					.catch((err) => {
 						if (err.response) {
@@ -206,6 +167,48 @@ export default class NewSongQuery extends Component {
 				alert("Ilyet nem játszunk.");
 			}
 		}
+	};
+
+	promptNoti = () => {
+		const firebaseConfig = {
+			apiKey: "AIzaSyCSnUYWR95Go_re_d6ClkK0AXgCXJJ5gNI",
+			authDomain: "playlist-248218.firebaseapp.com",
+			databaseURL: "https://playlist-248218.firebaseio.com",
+			projectId: "playlist-248218",
+			storageBucket: "",
+			messagingSenderId: "687275559983",
+			appId: "1:687275559983:web:12a27164caeadf2c3f8ada",
+			measurementId: "G-HZRTRM0P1J"
+		};
+		firebase.initializeApp(firebaseConfig);
+		const messaging = firebase.messaging();
+		messaging.usePublicVapidKey(
+			"BEumBgeaNS-cDLgwwjnUqKfKJs9l10mDn1-99N9RXY-0LaWGUA3I5vB_80k6jKWp2A61NzeM4siW6e1kF3uyzjc"
+		);
+		messaging
+			.requestPermission()
+			.then(() => {
+				console.log("Perm granted");
+				return messaging.getToken();
+			})
+			.then((token) => {
+				let url = "/api/setFcmToken/",
+					params = new URLSearchParams();
+				params.append("token", token);
+				axios.defaults.xsrfCookieName = "csrftoken";
+				axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+				axios
+					.post(url, params)
+					.then((res) => {
+						console.log("Token set");
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	render() {
