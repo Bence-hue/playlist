378d8afdca1646965cd415a0c1b9f368f71f332d
Tasztalos Pista <tasztalos69@gmail.com>
2019-12-25 18:56:54 +0100

    Arrange FCM code

--- src/pages/components/NewSongQuery.js
+++ src/pages/components/NewSongQuery.js
@@ -21,6 +21,7 @@ export default class NewSongQuery extends Component {
 		title: "",
 		toggleAnim: true,
 		toggleFinal: false,
+		toggleNotiRequest: false,
 		toggleErr: false,
 		err: {
 			title: "",
@@ -31,6 +32,63 @@ export default class NewSongQuery extends Component {
 
 	componentWillMount() {
 		this.setState({ csrfToken: cookie.load("csrftoken") });
+	}
+
+	componentDidMount() {
+		const firebaseConfig = {
+			apiKey: "AIzaSyCSnUYWR95Go_re_d6ClkK0AXgCXJJ5gNI",
+			authDomain: "playlist-248218.firebaseapp.com",
+			databaseURL: "https://playlist-248218.firebaseio.com",
+			projectId: "playlist-248218",
+			storageBucket: "",
+			messagingSenderId: "687275559983",
+			appId: "1:687275559983:web:12a27164caeadf2c3f8ada",
+			measurementId: "G-HZRTRM0P1J"
+		};
+		firebase.initializeApp(firebaseConfig);
+		const messaging = firebase.messaging();
+		messaging.usePublicVapidKey(
+			"BEumBgeaNS-cDLgwwjnUqKfKJs9l10mDn1-99N9RXY-0LaWGUA3I5vB_80k6jKWp2A61NzeM4siW6e1kF3uyzjc"
+		);
+		if (messaging.getToken()) {
+			this.setState({ allowNotiRequest: false });
+		} else {
+			this.setState({ allowNotiRequest: true });
+		}
+		this.promptNoti = () => {
+			console.log("function running");
+			messaging
+				.requestPermission()
+				.then(() => {
+					console.log("Perm granted");
+					return messaging.getToken();
+				})
+				.then((token) => {
+					let url = "/api/setFcmToken/",
+						params = new URLSearchParams();
+					params.append("token", token);
+					axios.defaults.xsrfCookieName = "csrftoken";
+					axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+					axios
+						.post(url, params)
+						.then((res) => {
+							console.log("Token set");
+							this.setState({ toggleNotiRequest: false });
+						})
+						.catch((err) => {
+							console.error(err);
+							alert("API Hiba!");
+							window.location.href = "/";
+						});
+				})
+				.catch((err) => {
+					if (!(err.name === "FirebaseError")) {
+						alert("Hiba!");
+						window.location.href = "/";
+					}
+					console.error(err);
+				});
+		};
 	}
 
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -77,6 +135,9 @@ export default class NewSongQuery extends Component {
 								toggleFinal: true,
 								toggleErr: false
 							});
+							if (this.state.allowNotiRequest) {
+								this.setState({ toggleNotiRequest: true });
+							}
 						}, 1000);
 					})
 					.catch((err) => {
@@ -169,60 +230,19 @@ export default class NewSongQuery extends Component {
 		}
 	};
 
-	promptNoti = () => {
-		console.log("function running");
-		const firebaseConfig = {
-			apiKey: "AIzaSyCSnUYWR95Go_re_d6ClkK0AXgCXJJ5gNI",
-			authDomain: "playlist-248218.firebaseapp.com",
-			databaseURL: "https://playlist-248218.firebaseio.com",
-			projectId: "playlist-248218",
-			storageBucket: "",
-			messagingSenderId: "687275559983",
-			appId: "1:687275559983:web:12a27164caeadf2c3f8ada",
-			measurementId: "G-HZRTRM0P1J"
-		};
-		firebase.initializeApp(firebaseConfig);
-		const messaging = firebase.messaging();
-		messaging.usePublicVapidKey(
-			"BEumBgeaNS-cDLgwwjnUqKfKJs9l10mDn1-99N9RXY-0LaWGUA3I5vB_80k6jKWp2A61NzeM4siW6e1kF3uyzjc"
-		);
-		messaging
-			.requestPermission()
-			.then(() => {
-				console.log("Perm granted");
-				return messaging.getToken();
-			})
-			.then((token) => {
-				let url = "/api/setFcmToken/",
-					params = new URLSearchParams();
-				params.append("token", token);
-				axios.defaults.xsrfCookieName = "csrftoken";
-				axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-				axios
-					.post(url, params)
-					.then((res) => {
-						console.log("Token set");
-					})
-					.catch((err) => {
-						console.log(err);
-					});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
 	render() {
 		return (
 			<React.Fragment>
 				<Modal
-					toggler={this.state.toggleFinal}
+					toggler={this.state.toggleNotiRequest}
 					title={"Értesítések"}
 					content={
 						"Ha szeretnél értesítést kapni, amikor a te zenédet játsszuk, kérlek fogadd el a következő felugró ablakban az értesítéseket."
 					}
 					buttonText={"OK"}
-					onButtonClick={this.promptNoti}
+					onButtonClick={() => {
+						this.promptNoti();
+					}}
 				/>
 				<Transition
 					native
