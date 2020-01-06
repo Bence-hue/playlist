c0c23c5ae06c4681860486e9c65640ef900f09cd
Aposztrof <42411122+Tasztalos69@users.noreply.github.com>
2020-01-06 12:09:39 +0100

    Try disabling sentry in dev

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import * as Sentry from "@sentry/browser";
 
-Sentry.init({
-	dsn: "https://97dc1883092644269366ced63680323b@sentry.io/1537907"
-});
+if(process.env.NODE_ENV !== "development") {
+	Sentry.init({
+		dsn: "https://97dc1883092644269366ced63680323b@sentry.io/1537907"
+	});
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
